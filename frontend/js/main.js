2b8b55f929078772dec6bba3a1fa54cbfc17ef87
Anadi Singh <anadisingh4969@gmail.com>
2026-05-25 01:23:03 +0530

    feat : romavision frontend

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://romavision.onrender.com/api';
 
 // Get Token from localStorage
 const getToken = () => localStorage.getItem('token');
